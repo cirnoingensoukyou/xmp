36d414000b3bda44fe4509589273a26693807607
Cirno <linux777888@gmail.com>
2019-10-18 00:33:29 +0900

    20191018_00-33-29

--- linestampkiller.js
+++ linestampkiller.js
@@ -2,13 +2,13 @@
 
 const checkUrl = url =>
 {
-    let aTag = $('<a href="' + url + '"></a>')[0];
+    const aTag = $('<a href="' + url + '"></a>')[0];
 
-    let pathArr = aTag.pathname.split('/').filter(_ => _ !== '');
+    const pathArr = aTag.pathname.split('/').filter(_ => _ !== '');
     if (pathArr.length < 3)
         return false;
 
-    let r = (aTag.protocol === 'https:' || aTag.protocol === 'http:') && 
+    const r = (aTag.protocol === 'https:' || aTag.protocol === 'http:') && 
         aTag.hostname === 'store.line.me' &&
         pathArr[0] === 'stickershop' &&
         pathArr[1] === 'product';
